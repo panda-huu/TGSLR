12dc8ee1bc000279466adb7bd0c7c6fe191edc51
panda-huu <gurjasharma3@gmail.com>
2026-02-15 13:20:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
 
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
+
+MERCHANT_KEY = os.getenv("MERCHANT_KEY")
+LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))
+DEPOSIT_LOG_ID = int(os.getenv("DEPOSIT_LOG_ID"))
